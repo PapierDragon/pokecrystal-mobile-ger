b5e7419950303464aee6d8ec45c70dc2623cf78f
pokegold-spaceworld <110418063+gb-mobile@users.noreply.github.com>
2024-01-19 17:17:51 -0500

    Update mobile_5c.asm

--- mobile/mobile_5c.asm
+++ mobile/mobile_5c.asm
@@ -776,9 +776,9 @@ INCBIN "gfx/pokedex/slowpoke_mobile.2bpp.lz"
 String_172e31:
 	db "PASSWORT-Eingabe@";"パスワード<WO>いれてください@"
 String_172e3f:
-	db " MEHR ZUR. O.K.@";"きりかえ　やめる　　けってい@"
+	db "UMSCH ZUR. O.K.@";"きりかえ　やめる　　けってい@"
 String_172e4e:
-	db " MEHR ZUR. O.K.@";"きりかえ　やめる　　"
+	db "UMSCH ZUR. O.K.@";"きりかえ　やめる　　"
 String_172e58:
 	db "O.K.@";"けってい@"
 String_172e5d:
