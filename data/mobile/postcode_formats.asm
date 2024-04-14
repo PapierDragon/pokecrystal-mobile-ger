f108ceff89bb58d8512b48d85955ae578e07ff5b
gb-mobile <110418063+gb-mobile@users.noreply.github.com>
2024-04-14 18:48:25 -0400

    Update to new Zip Code format

---
+++ data/mobile/postcode_formats.asm
@@ -0,0 +1,8 @@
+Zipcode_CharPool_Formats:
+	dw Zipcode_CharPools_Format0
+;________________________________
+Zipcode_CharPools_Format0:          ; 0-9   0-9   0-9   0-9   0-9   0-9
+	db CHARPOOL_0_TO_9
+	db CHARPOOL_0_TO_9
+	db CHARPOOL_0_TO_9
+	db CHARPOOL_0_TO_9
---
+++ data/mobile/prefecture_to_country_pairs.asm
@@ -0,0 +1,77 @@
+PrefectureToCountry:
+	db 0    ; Brandenburg
+	db 0    ; Berlin
+	db 0    ; Baden-Württemberg
+	db 0    ; Bayern
+	db 0    ; Bremen
+	db 0    ; Hessen
+	db 0    ; Hamburg
+	db 0    ; Mecklenburg-Vorpommern
+	db 0    ; Niedersachsen
+	db 0    ; Nordrhein-Westfalen
+	db 0    ; Rheinland-Pfalz
+	db 0    ; Schleswig-Holstein
+	db 0    ; Saarland
+	db 0    ; Sachsen
+	db 0    ; Sachsen-Anhalt
+	db 0    ; Thüringen
+	db 1      ; Burgenland
+	db 1      ; Carinthia
+	db 1      ; Lower Austria
+	db 1      ; Upper Austria
+	db 1      ; Salzburg
+	db 1      ; Styria
+	db 1      ; Tyrol
+	db 1      ; Vorarlberg
+	db 1      ; Vienna
+	db 2     ; Aargau
+	db 2     ; Appenzell Innerrhoden
+	db 2     ; Appenzell Ausserrhoden
+	db 2     ; Berne
+	db 2     ; Basel-Landschaft
+	db 2     ; Basel-Stadt
+	db 2     ; Fribourg
+	db 2     ; Genève
+	db 2     ; Glarus
+	db 2     ; Graubünden
+	db 2     ; Jura
+	db 2     ; Luzer
+	db 2     ; Neuchâtel
+	db 2     ; Nidwalden
+	db 2     ; Obwalden
+	db 2     ; Sankt Gallen
+	db 2     ; Schaffhausen
+	db 2     ; Solothurn
+	db 2     ; Schwyz
+	db 2     ; Thurgau
+	db 2     ; Ticino
+	db 2     ; Uri
+	db 2     ; Vaud
+	db 2     ; Valais
+	db 2     ; Zug
+	db 2     ; Zürich
+	db 3     ; Balzers
+	db 3     ; Eschen
+	db 3     ; Gamprin
+	db 3     ; Mauren
+	db 3     ; Planken
+	db 3     ; Ruggell
+	db 3     ; Schaan
+	db 3     ; Schellenberg
+	db 3     ; Triesen
+	db 3     ; Triesenberg
+	db 3     ; Vaduz
+	db 4    ; Capellen
+	db 4    ; Clervaux
+	db 4    ; Diekirch
+	db 4    ; Echternach
+	db 4    ; Esch-sur-Alzette
+	db 4    ; Grevenmacher
+	db 4    ; Luxembourg
+	db 4    ; Mersch
+	db 4    ; Redange
+	db 4    ; Remich
+	db 4    ; Vianden
+	db 4    ; Wiltz
+
+	
