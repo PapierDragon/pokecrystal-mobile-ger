1a830de1170ff3433b5527142d3eb45888e52e21
gb-mobile <110418063+gb-mobile@users.noreply.github.com>
2024-11-16 03:30:15 -0500

    Updated Location Comments

--- data/mobile/prefectures_list.asm
+++ data/mobile/prefectures_list.asm
@@ -1,55 +1,55 @@
 Prefectures: ; Some names shortened to fit, check for official initials later. In this list, strings must be right-aligned.
-	db "DE-BW@"
-	db "DE-BY@"
-	db "DE-BE@"
-	db "DE-BB@"
-	db "DE-HB@"
-	db "DE-HH@"
-	db "DE-HE@"
-	db "DE-MV@"
-	db "DE-NI@"
-	db "DE-NW@"
-	db "DE-RP@"
-	db "DE-SL@"
-	db "DE-SN@"
-	db "DE-ST@"
-	db "DE-SH@"
-	db "DE-TH@"
-	db "AT-1 @"
-	db "AT-2 @"
-	db "AT-3 @"
-	db "AT-4 @"
-	db "AT-5 @"
-	db "AT-6 @"
-	db "AT-7 @"
-	db "AT-8 @"
-	db "AT-9 @"
-	db "CH-ZH@"
-	db "CH-BE@"
-	db "CH-LU@"
-	db "CH-UR@"
-	db "CH-SZ@"
-	db "CH-OW@"
-	db "CH-NW@"
-	db "CH-GL@"
-	db "CH-ZG@"
-	db "CH-FR@"
-	db "CH-SO@"
-	db "CH-BS@"
-	db "CH-BL@"
-	db "CH-SH@"
-	db "CH-AR@"
-	db "CH-AI@"
-	db "CH-SG@"
-	db "CH-GR@"
-	db "CH-AG@"
-	db "CH-TG@"
-	db "CH-TI@"
-	db "CH-VD@"
-	db "CH-VS@"
-	db "CH-NE@"
-	db "CH-GE@"
-	db "CH-JU@"
+	db "DE-BW@"		; Baden-Württemberg
+	db "DE-BY@"		; Bavaria
+	db "DE-BE@"		; Berlin
+	db "DE-BB@"		; Brandenburg
+	db "DE-HB@"		; Bremen
+	db "DE-HH@"		; Hamburg
+	db "DE-HE@"		; Hesse
+	db "DE-MV@"		; Lower Saxony
+	db "DE-NI@"		; Mecklenburg-Vorpommern
+	db "DE-NW@"		; North Rhine-Westphalia
+	db "DE-RP@"		; Rhineland-Palatinate
+	db "DE-SL@"		; Saarland
+	db "DE-SN@"		; Saxony
+	db "DE-ST@"		; Saxony-Anhalt
+	db "DE-SH@"		; Schleswig-Holstein
+	db "DE-TH@"		; Thuringia
+	db "AT-1 @"		; Burgenland
+	db "AT-2 @"		; Kärnten
+	db "AT-3 @"		; Niederösterreich
+	db "AT-4 @"		; Oberösterreich
+	db "AT-5 @"		; Salzburg
+	db "AT-6 @"		; Steiermark
+	db "AT-7 @"		; Tirol
+	db "AT-8 @"		; Vorarlberg
+	db "AT-9 @"		; Wien
+	db "CH-ZH@"	; Zürich
+	db "CH-BE@"	; Bern / Berne
+	db "CH-LU@"	; Luzern
+	db "CH-UR@"	; Uri
+	db "CH-SZ@"	; Schwyz
+	db "CH-OW@"	; Obwalden
+	db "CH-NW@"	; Nidwalden
+	db "CH-GL@"	; Glarus
+	db "CH-ZG@"	; Zug
+	db "CH-FR@"	; Freiburg / Fribourg
+	db "CH-SO@"	; Solothurn
+	db "CH-BS@"	; Basel Stadt
+	db "CH-BL@"	; Basel Land
+	db "CH-SH@"	; Schaffhausen
+	db "CH-AR@"	; Appenzell Ausserrhoden
+	db "CH-AI@"	; Appenzell Innerrhoden
+	db "CH-SG@"	; Sankt Gallen
+	db "CH-GR@"	; Graubünden
+	db "CH-AG@"	; Aargau
+	db "CH-TG@"	; Thurgau
+	db "CH-TI@"	; Ticino
+	db "CH-VD@"	; Vaud
+	db "CH-VS@"	; Valais / Wallis
+	db "CH-NE@"	; Neuchâtel
+	db "CH-GE@"	; Genève
+	db "CH-JU@"	; Jura
 	db "LI-01@"      ; Balzers
 	db "LI-02@"      ; Eschen
 	db "LI-03@"      ; Gamprin
